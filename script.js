3165c7bc38ec2e4ed16b4750aae266e712eb5b07
Olaniyi Mercy <olaniyimercy45@gmail.com>
2025-07-17 10:45:19 +0100

    done

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const strength = document.getElementById('displayStrength')
 const sent = document.getElementById('submit')
 
      if (passWord.length < 5) {
-            displayStrength.value = 'Weak...'
+            displayStrength.value = 'Weak'
             displayStrength.style.color = 'red'
         } else if (passWord.length < 7) {
-           displayStrength.value = 'Medium...'
+           displayStrength.value = 'Medium'
            displayStrength.style.color = 'yellow'
         } else {
-            displayStrength.value = 'Strong...'
+            displayStrength.value = 'Strong'
             displayStrength.style.color = 'green'
         }
 }
