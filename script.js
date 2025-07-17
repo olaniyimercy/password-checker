098aa899ca91d32f5e51f9c064fc718b4670e08b
Olaniyi Mercy <olaniyimercy45@gmail.com>
2025-07-17 09:39:02 +0100

    done

---
+++ script.js
@@ -0,0 +1,16 @@
+function checkp() {
+const passWord = document.getElementById('password').value;
+const strength = document.getElementById('displayStrength')
+const sent = document.getElementById('submit')
+
+     if (passWord.length < 5) {
+            displayStrength.value = 'Weak...'
+            displayStrength.style.color = 'red'
+        } else if (passWord.length < 7) {
+           displayStrength.value = 'Medium...'
+           displayStrength.style.color = 'yellow'
+        } else {
+            displayStrength.value = 'Strong...'
+            displayStrength.style.color = 'green'
+        }
+}
